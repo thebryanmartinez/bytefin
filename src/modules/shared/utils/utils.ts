1d066cbef74513764d1c5152b25a6b8e8e6207f2
Bryan Martínez <bryanmartz972@gmail.com>
2026-01-27 00:11:46 -0600

    feat: refactor accounts and funds services from Convex

--- src/modules/shared/utils/utils.ts
+++ src/modules/shared/utils/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getNewBalance = (currentBalance: number, amount: number) => {
+  return currentBalance + amount;
+};
