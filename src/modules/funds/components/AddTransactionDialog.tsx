596d55885edab0f21117279c6d4fb0c6053f48db
Bryan Martínez <bryanmartz972@gmail.com>
2026-01-27 10:20:02 -0600

    chore: remove unused imports

--- src/modules/funds/components/AddTransactionDialog.tsx
+++ src/modules/funds/components/AddTransactionDialog.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import type { Id } from "@convex/_generated/dataModel";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Plus } from "lucide-react";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller } from "react-hook-form";
 import * as z from "zod";
 import {
   Button,
